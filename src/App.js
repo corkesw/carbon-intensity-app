888547e0a95cb67720b9baddb70d21db22e4a61b
corkesw <home@stephencorke.com>
2021-10-11 13:00:29 +0100

    add postcode search to SearchBox

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Header from './components/Header';
 import ResultsDisplay from './components/ResultsDisplay';
 import SearchBox from './components/SearchBox';
+import {useState} from 'react';
 
 function App() {
+  const [postcode, setPostcode] = useState('')
+  console.log('postcode >>>>', postcode)
   return (
     <div className="App">
       <Header />
-      <SearchBox />
+      <SearchBox setPostcode={setPostcode}/>
       <ResultsDisplay />
     </div>
   );
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,14 +1,42 @@
-function SearchBox () {
+import { useState } from "react";
+
+function SearchBox ({setPostcode}) {
+    
+    const postcodeRegex = /^[a-z]{1,2}\d{1,2}$/i
+    const [postcodeError, setPostcodeError] = useState(false)
+    const [postcodeSearch, setPostcodeSearch] = useState('')
+
+    function updatePostcode (e) {
+        setPostcodeSearch(e.target.value)
+    }
+
+    function handleSubmit (e) {
+        e.preventDefault()
+        setPostcodeError(false)
+
+        if (postcodeRegex.test(postcodeSearch)) {
+
+            setPostcode(postcodeSearch) 
+
+        } else {
+
+            setPostcodeError(true)
+        }
+       
+
+        
+    }
+
     return (
         <section className="search">
-            <form action="submit">
+            <form onSubmit={handleSubmit} action="submit">
                 <label htmlFor="postcodeInput">Postcode: </label>
-                <input id="postcodeInput" type="text" required></input>
+                <input id="postcodeInput" type="text" required onChange={updatePostcode} value={postcodeSearch}></input>
                 <label htmlFor="dateInput">Date: </label>
                 <input id="dateInput" type="date"></input>
-                <button id="submitButton" class="button">Submit</button>
+                <button id="submitButton" className="button">Submit</button>
             </form>
-
+            <p className={`iserror${postcodeError}`}>Please enter valid postcode</p>
         </section>
     )
 }
